93390ee03bc77a828e38e2e90d2819154bd5c56d
Michael Dr <dryzuo@gmail.com>
2016-06-05 16:21:51 +0300

    DAL BLL add

---
+++ BLL/IService.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IService.cs" company="">
+//   
+// </copyright>
+// <summary>
+// Base interface for all services
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BLL
+{
+	using DAL;
+
+	/// <summary>
+	/// Base interface for all services
+	/// </summary>
+	public interface IService
+	{
+		#region Public Properties
+
+		/// <summary>
+		/// UnitOfWork
+		/// </summary>
+		IUnitOfWork unitOfWork { get; set; }
+
+		#endregion
+	}
+}
